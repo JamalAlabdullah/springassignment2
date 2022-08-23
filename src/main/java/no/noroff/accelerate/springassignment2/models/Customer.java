0929b9052a922b96f55c63e549ce221588f76a0f
nma044 <nathanielm@lyse.net>
2022-08-23 14:22:29 +0200

    Setup postgres and added record for customer

---
+++ src/main/java/no/noroff/accelerate/springassignment2/models/Customer.java
@@ -0,0 +1,4 @@
+package no.noroff.accelerate.springassignment2.models;
+
+public record Customer(int id, String firstName, String lastName, String country, String postalCode, String phoneNumber, String email) {
+}
