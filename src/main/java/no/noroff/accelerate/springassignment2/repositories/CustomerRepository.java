d1d40ae1f3c2fe3d97beb5736c7ccd181be94bf1
nma044 <nathanielm@lyse.net>
2022-08-24 13:59:18 +0200

    Added findByLimitOffset

--- src/main/java/no/noroff/accelerate/springassignment2/repositories/CustomerRepository.java
+++ src/main/java/no/noroff/accelerate/springassignment2/repositories/CustomerRepository.java
@@ -3,11 +3,14 @@ package no.noroff.accelerate.springassignment2.repositories;
 import no.noroff.accelerate.springassignment2.models.Customer;
 
 import java.sql.ResultSet;
+import java.util.List;
 
 public interface CustomerRepository extends CRUDRepository{
 
 
     Customer findByName(String firstName);
+
+    List<Customer> findLimitOffset(int limit, int offset);
 
     int insert(Customer customer);
 
