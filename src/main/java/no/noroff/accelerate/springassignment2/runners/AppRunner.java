e30a740d63b34cc8fd3df63de61fe5b206dad49c
Jamal Alabdullah <jamal.alabdullah@no.experis.com>
2022-08-25 13:44:36 +0200

    CHORE:Finished appRunner

--- src/main/java/no/noroff/accelerate/springassignment2/runners/AppRunner.java
+++ src/main/java/no/noroff/accelerate/springassignment2/runners/AppRunner.java
@@ -1,6 +1,7 @@
 package no.noroff.accelerate.springassignment2.runners;
 
 import no.noroff.accelerate.springassignment2.dataaccess.CustomerRepositoryImpl;
+import no.noroff.accelerate.springassignment2.models.Customer;
 import no.noroff.accelerate.springassignment2.repositories.CustomerRepository;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -19,6 +20,16 @@ public class AppRunner implements ApplicationRunner {
         CustomerRepositoryImpl customerRepository= new CustomerRepositoryImpl();
         System.out.println( customerRepository.totalSpender());
         customerRepository.findAll();
+        customerRepository.countryMostCustomer();
+        customerRepository.update(new Object());
+        customerRepository.findById(3);
+        customerRepository.findByName("Emma");
+        customerRepository.findLimitOffset(4,1);
+        customerRepository.insert(new Customer(60,"Jamal/Nathaniel","Abdullah/Maaskant",
+                "Norway","1234","123456","asd@asdd"));
+
+
+
 
     }
 }
